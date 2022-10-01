39fa7b45a849b56bc9ced90b1354060d703659de
Juan24rangel <juan2224658@correo.uis.edu.co>
2022-10-01 07:44:07 -0500

    altura

--- pelota.py
+++ pelota.py
@@ -1,4 +1,16 @@
 # Una pelota de deja caer de una altura h, y en cada rebote sube el 10% menos del anterior. Hacer el diagrama de flujo y el programa en Python, que le h y que calculé e imprima en cual rebote la pelota no alcanza a subir la quinta parte de la altura inicial.
 
-## analisis:
-![Diagrama de flujo](pelota.png ("Diagrama de flujo"))
+#input
+
+H = int(input("Digite la altura:"))
+
+#processing
+Q= H / 5
+r = 0
+while H>Q:
+    H = H - (H * 0.1)
+    r = r+1
+    
+#output
+print("Numero de rebotes: "+str(r))
+    
